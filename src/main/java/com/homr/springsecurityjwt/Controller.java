825ad7e80d10c852d17b9d73f7bca4b1e6ddb2cf
shashwatmrsn <shashwatmrsn3@gmail.com>
2020-05-15 12:46:27 +0545

    initial commit

---
+++ src/main/java/com/homr/springsecurityjwt/Controller.java
@@ -0,0 +1,13 @@
+package com.homr.springsecurityjwt;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+        
+    }
+}
